3fd83342586fdd75c2a303b1e25ffe665c749850
gipfeli <nguyenchanhdat@gmail.com>
2015-09-11 02:29:10 +0200

    Add index, offer, kontakt and recruit

---
+++ app/helpers/spod_helper.rb
@@ -0,0 +1,2 @@
+module SpodHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  get 'spod/index'
+
+  get 'spod/contact'
+
+  get 'spod/offer'
+
+  get 'spod/mitarbeit'
+
+  root 'spod#index'
 end
